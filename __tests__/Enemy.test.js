fcab9fef2752c6eec45aab34958e3cb4004e3abf
paperpatch <patchen21@gmail.com>
2021-11-17 15:28:35 -0500

    10.3

--- __tests__/Enemy.test.js
+++ __tests__/Enemy.test.js
@@ -1,5 +1,5 @@
-const Enemy = require('../lib/Enemy')
-const Potion = require('../lib/Potion')
+const Enemy = require('../lib/Enemy.js');
+const Potion = require('../lib/Potion.js');
 
 jest.mock('../lib/Potion.js');
 
@@ -18,6 +18,13 @@ test("gets enemy's health value", () => {
   const enemy = new Enemy('goblin', 'sword');
 
   expect(enemy.getHealth()).toEqual(expect.stringContaining(enemy.health.toString()));
+});
+
+test('gets a description of the enemy', () => {
+  const enemy = new Enemy('goblin', 'sword');
+
+  expect(enemy.getDescription()).toEqual(expect.stringContaining('goblin'));
+  expect(enemy.getDescription()).toEqual(expect.stringContaining('sword'));
 });
 
 test('checks if enemy is alive or not', () => {
@@ -50,10 +57,3 @@ test("subtracts from enemy's health", () => {
 
   expect(enemy.health).toBe(0);
 });
-
-test('gets a description of the enemy', () => {
-  const enemy = new Enemy('goblin', 'sword');
-
-  expect(enemy.getDescription()).toEqual(expect.stringContaining('goblin'));
-  expect(enemy.getDescription()).toEqual(expect.stringContaining('sword'));
-})
